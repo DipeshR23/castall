f1b6b531205233de4efd3759a12c9d8e7c795131
DipeshR23 <rdtech9347@gmail.com>
2026-08-07 21:28:01 +0545

    revert: remove background effects from landing page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 import { Monitor, Smartphone, Shield, MonitorPlay } from 'lucide-react';
-import BackgroundEffects from '../components/presentation/BackgroundEffects.js';
 
 export default function LandingPage() {
   const navigate = useNavigate();
@@ -24,8 +23,7 @@ export default function LandingPage() {
   ];
 
   return (
-    <div className="relative flex flex-col overflow-hidden">
-      <BackgroundEffects smokeOnly />
+    <div className="flex flex-col">
       {/* Hero Section */}
       <section className="relative flex flex-col items-center text-center pt-10 sm:pt-14 md:pt-20 lg:pt-24 pb-12 sm:pb-16 md:pb-20 lg:pb-24 px-4">
 
