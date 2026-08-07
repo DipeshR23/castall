2d802c7c584a7707c98c13225022771335164b1d
DipeshR23 <rdtech9347@gmail.com>
2026-08-07 16:40:47 +0545

    revert: restore previous SharePage UI before background-effects change

--- frontend/src/pages/SharePage.tsx
+++ frontend/src/pages/SharePage.tsx
@@ -3,7 +3,6 @@ import { toast } from 'sonner';
 import JoinCard from '../components/room/JoinCard';
 import WaitingForApproval from '../components/room/WaitingForApproval';
 import StartSharing from '../components/presentation/StartSharing';
-import BackgroundEffects from '../components/presentation/BackgroundEffects.js';
 import ConnectionStatusButton from '../components/ui/ConnectionStatusButton.js';
 import { useRoom } from '../contexts/RoomContext.js';
 import { useSocket } from '../hooks/useSocket.js';
@@ -93,14 +92,13 @@ export default function SharePage() {
   if (step === 'waiting') {
     return (
       <div className="relative flex items-center justify-center min-h-[calc(100vh-72px)] px-[5%] py-8 sm:py-12">
-        <BackgroundEffects />
-        <div className="absolute top-4 left-4 sm:top-6 sm:left-6 z-20">
+        <div className="absolute top-4 left-4 sm:top-6 sm:left-6">
           <ConnectionStatusButton connected={isConnected} />
         </div>
-        <div className="absolute top-4 right-4 sm:top-6 sm:right-6 z-20">
+        <div className="absolute top-4 right-4 sm:top-6 sm:right-6">
           <ConnectionStatusButton connected={false} label="Exit" onClick={handleExit} icon={<LogOut className="h-5 w-5" />} hoverable />
         </div>
-        <div className="relative z-10 w-full max-w-7xl">
+        <div className="w-full max-w-7xl">
           <WaitingForApproval />
         </div>
       </div>
@@ -110,14 +108,13 @@ export default function SharePage() {
   if (step === 'sharing') {
     return (
       <div className="relative flex items-center justify-center min-h-[calc(100vh-72px)] px-[5%] py-8 sm:py-12">
-        <BackgroundEffects />
-        <div className="absolute top-4 left-4 sm:top-6 sm:left-6 z-20">
+        <div className="absolute top-4 left-4 sm:top-6 sm:left-6">
           <ConnectionStatusButton connected={isConnected} />
         </div>
-        <div className="absolute top-4 right-4 sm:top-6 sm:right-6 z-20">
+        <div className="absolute top-4 right-4 sm:top-6 sm:right-6">
           <ConnectionStatusButton connected={false} label="Exit" onClick={handleExit} icon={<LogOut className="h-5 w-5" />} hoverable />
         </div>
-        <div className="relative z-10 w-full max-w-7xl">
+        <div className="w-full max-w-7xl">
           <StartSharing />
         </div>
       </div>
@@ -126,7 +123,6 @@ export default function SharePage() {
 
   return (
     <div className="relative flex items-center justify-center min-h-[calc(100vh-72px)] px-[5%] py-8 sm:py-12">
-      <BackgroundEffects />
       <div className="absolute top-4 left-4 sm:top-6 sm:left-6">
         <ConnectionStatusButton connected={isConnected} />
       </div>
@@ -143,4 +139,3 @@ export default function SharePage() {
     </div>
   );
 }
-
