cd9470da6c89ca50848a124653580f28c1e7c57b
DipeshR23 <rdtech9347@gmail.com>
2026-08-08 22:35:41 +0545

    fix: switch back to BrowserRouter with _redirects SPA fallback

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import App from './App';
 import { RoomProvider } from './contexts/RoomContext.js';
@@ -9,13 +9,13 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <ThemeProvider>
         <RoomProvider>
           <App />
         </RoomProvider>
       </ThemeProvider>
       <Toaster position="top-right" richColors closeButton />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
